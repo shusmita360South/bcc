69a63bcc988f0dbc74f032056dc08bd08ed88238
Ed-360South <ed@360south.com.au>
2021-04-27 15:01:21 +1000

    Added Donate Page Added Mobile Responsive buttons for donate in header
    Signed-off-by: Ed-360South <ed@360south.com.au>

---
+++ wp-content/themes/baysidecommunity/page-donate.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Template Name: Page Donate
+ *
+ *
+ * @package WordPress
+ * @subpackage Bayside Community
+ * @since 1.0.0
+ */
+
+
+get_header();
+$peoples = get_people();
+?>
+<section class="page-default container">
+	<div class="">
+		<?php get_template_part( 'template-parts/header/entry-header' ); ?>
+	</div>
+	<?php
+	/* Start the Loop */
+	while ( have_posts() ) :
+		the_post();
+		//get_template_part( 'template-parts/content/content', 'pagecontent' );
+	endwhile; // End of the loop.
+	?>
+
+	<section class="donate-archive section-padding-tb light-bg">
+		<div class="grid-container">
+			<div uk-grid>
+				
+				<?php if ($peoples->have_posts()) :
+					$count = 1;
+					
+						while ( $peoples->have_posts() ) {
+							$peoples->the_post();
+							get_template_part( 'template-parts/content/content', 'people' );
+						}
+						wp_reset_postdata(); 
+				endif;?>
+			</div>		
+		</div>		
+			
+	</section>
+
+	<?php if ( get_field('ctabottom_heading') ) : ?>
+		<section class="block-1 center section-padding-tb ctabottom-bg">
+			<div class="grid-container-small">
+				<div class="block-1-inner">
+					<h2 class="white"><?php the_field('ctabottom_heading') ?></h2>
+					<p class="white short-desc"><?php echo nl2br(get_field('ctabottom_short_description')) ?></p>
+					<a class="button btn-white uk-margin-medium-top" href="<?php echo get_the_permalink(get_field('ctabottom_button_link')) ?>"><?php the_field('ctabottom_button_text') ?></a>
+
+
+				</div>
+			</div>
+		</section>
+	<?php endif; ?>
+
+</section>	
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+<?php
+get_footer();
--- wp-content/themes/baysidecommunity/template-parts/modal-menu.php
+++ wp-content/themes/baysidecommunity/template-parts/modal-menu.php
@@ -137,9 +137,24 @@
 						</ul>
 					</nav><!-- .social-menu -->
 
+
+					<?php 
+
+						$header_donate_button_url = get_option('header_donate_button_url');
+						if ($header_donate_button_url != "") {
+					?>
+							<div class="mobile-extra-buttons">
+								<a class="donate" href="<?php echo $header_donate_button_url; ?>"><span uk-icon="icon: heart" class="uk-icon"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="heart"><path fill="none" stroke="#000" stroke-width="1.03" d="M10,4 C10,4 8.1,2 5.74,2 C3.38,2 1,3.55 1,6.73 C1,8.84 2.67,10.44 2.67,10.44 L10,18 L17.33,10.44 C17.33,10.44 19,8.84 19,6.73 C19,3.55 16.62,2 14.26,2 C11.9,2 10,4 10,4 L10,4 Z"></path></svg></span>Donate</a>
+							</div>
+					<?php 
+						}
+					?>
+
 				<?php } ?>
 
 			</div><!-- .menu-bottom -->
+
+			
 
 		</div><!-- .menu-wrapper -->
 
